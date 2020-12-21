50dade8a98e392e1d3c27d1d4e5d4906251a64f7
Clarisse-3924 <cuwizeyimana3924@gmail.com>
2020-12-21 13:56:34 +0200

    adding photoes

--- app/src/main/java/com/example/smartbabies/MainActivity.java
+++ app/src/main/java/com/example/smartbabies/MainActivity.java
@@ -3,12 +3,19 @@ package com.example.smartbabies;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
-
+List<listview>list;
+ListView list_item;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        list=new ArrayList<>();
+
     }
 }
