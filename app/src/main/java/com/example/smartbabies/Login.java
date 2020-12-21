3636e79be170a33760ee7264ab9370f4718e1df1
Clarisse-3924 <cuwizeyimana3924@gmail.com>
2020-12-21 14:58:58 +0200

    creating

--- app/src/main/java/com/example/smartbabies/Login.java
+++ app/src/main/java/com/example/smartbabies/Login.java
@@ -1,12 +1,18 @@
 package com.example.smartbabies;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class Login extends AppCompatActivity {
     @BindView(R.id.email1) EditText textEmail;
@@ -17,5 +23,53 @@ public class Login extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        ButterKnife.bind(this);
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String email = textEmail.getText().toString().trim();
+                String password = textpass.getText().toString().trim();
+
+                if (TextUtils.isEmpty(email)) {
+                    textEmail.setError("email required");
+                    return;
+                }
+
+                if (TextUtils.isEmpty(password)) {
+                    textpass.setError("password is empty");
+                    return;
+
+                }
+                if (password.length() < 6) {
+                    textpass.setError("password must be 6 characters");
+                    return;
+                }
+                progressBar.setVisibility(View.VISIBLE);
+                //Authenticate the user
+                fAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            Toast.makeText(Login.this, "you have succesfully logged", Toast.LENGTH_SHORT).show();
+                            startActivity(new Intent(getApplicationContext(), Signup.class));
+                        } else {
+                            Toast.makeText(Login.this, "error occured" + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                            progressBar.setVisibility(View.GONE);
+                        }
+                    }
+                });
+
+
+            }
+        });
+        create.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(), MainActivity.class));
+
+            }
+        });
+
     }
 }
