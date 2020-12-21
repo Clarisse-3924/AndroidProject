874e44300d0a5bddbb607b5b2780c7fc73cd2350
Clarisse-3924 <cuwizeyimana3924@gmail.com>
2020-12-21 13:05:27 +0200

    creating a constructor

--- app/src/main/java/com/example/smartbabies/listview.java
+++ app/src/main/java/com/example/smartbabies/listview.java
@@ -1,4 +1,12 @@
 package com.example.smartbabies;
 
 public class listview {
+    int image;
+    String name,addToCart;
+
+    public listview(int image, String name, String addToCart) {
+        this.image = image;
+        this.name = name;
+        this.addToCart = addToCart;
+    }
 }
