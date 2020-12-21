97f152e5e1902453fdac9c7dc7839a1ac6beadd3
Clarisse-3924 <cuwizeyimana3924@gmail.com>
2020-12-21 13:22:24 +0200

    getting the views

--- app/src/main/java/com/example/smartbabies/mylistadapter.java
+++ app/src/main/java/com/example/smartbabies/mylistadapter.java
@@ -1,5 +1,29 @@
 package com.example.smartbabies;
 
-public class mylistadapter {
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import java.util.List;
+
+public class mylistadapter extends ArrayAdapter<listview> {
+    Context context;
+    int resource;
+    List<listview>list;
+    public mylistadapter(Context context, int resource, List<listview>list){
+        super(context, resource, list);
+        this.context=context;
+        this.resource=resource;
+        this.list=list;
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        return super.getView(position, convertView, parent);
+    }
 }
